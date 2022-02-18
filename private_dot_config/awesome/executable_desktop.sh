87f120c6baf21642f5af495f92989f0200d5b5ba
Johan Sandström <loxley@users.noreply.github.com>
2022-02-18 21:23:22 +0100

    Change awesome xrandr

--- private_dot_config/awesome/executable_desktop.sh
+++ private_dot_config/awesome/executable_desktop.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-xrandr --output DP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --off --output VIRTUAL1 --off
+xrandr --output DP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --off --output VIRTUAL1 --off
+#xrandr --output DP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off --output HDMI1 --off --output VIRTUAL1 --off
