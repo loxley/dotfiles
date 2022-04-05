41f4a261de8555fcb2f9f4065db91411286ae877
Johan Sandström <loxley@users.noreply.github.com>
2022-04-05 22:42:44 +0200

    Add scratchpad

--- private_dot_config/awesome/keys.lua
+++ private_dot_config/awesome/keys.lua
@@ -19,6 +19,18 @@ root.buttons(gears.table.join(
     awful.button({ }, 5, awful.tag.viewprev)
 ))
 
+local term_scratch = bling.module.scratchpad {
+    command = "alacritty --class spad",               -- How to spawn the scratchpad
+    rule = { instance = "spad" },                     -- The rule that the scratchpad will be searched by
+    sticky = true,                                    -- Whether the scratchpad should be sticky
+    autoclose = true,                                 -- Whether it should hide itself when losing focus
+    floating = true,                                  -- Whether it should be floating (MUST BE TRUE FOR ANIMATIONS)
+    geometry = {x=360, y=90, height=900, width=1200}, -- The geometry in a floating state
+    reapply = false,                                   -- Whether all those properties should be reapplied on every new opening of the scratchpad (MUST BE TRUE FOR ANIMATIONS)
+    dont_focus_before_close  = false,                 -- When set to true, the scratchpad will be closed by the toggle function regardless of whether its focused or not. When set to false, the toggle function will first bring the scratchpad into focus and only close it on a second call
+    rubato = {x = anim_x, y = anim_y}                 -- Optional. This is how you can pass in the rubato tables for animations. If you don't want animations, you can ignore this option.
+}
+
 -- Globals
 local globalkeys = gears.table.join(
     
@@ -98,6 +110,8 @@ local globalkeys = gears.table.join(
               {description = "monocle (max) mode", group = "layout"}),
 
     -- Group: Launcher
+    awful.key({ modkey, "Shift"   }, "p", function () term_scratch:toggle()         end,
+              {description = "scratchpad", group = "launcher"}),
     awful.key({ modkey,           }, "Return", function () awful.spawn("alacritty") end,
               {description = "open a terminal", group = "launcher"}),
     awful.key({ modkey,           }, "d",      function () awful.spawn(launcher)    end,
