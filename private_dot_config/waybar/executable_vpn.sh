1737a513dfa7b009781a291312289f59f28bda34
Johan Sandström <loxley@users.noreply.github.com>
2024-04-11 10:18:42 +0200

    Fix some stuff hyprland and waybar

--- private_dot_config/waybar/executable_vpn.sh
+++ private_dot_config/waybar/executable_vpn.sh
@@ -2,7 +2,7 @@
 VPN=$(nmcli connection show --active | grep -e wireguard -e Prisjakt |
       cut -d ' ' -f1 | tr '[:upper:]' '[:lower:]')
 if [ -z "$VPN" ]; then
-  echo "{\"text\":\"  no vpn\"}"
+  echo "{\"text\":\"<span color='#ff5370'>  no vpn</span>\"}"
 else
-  echo "{\"text\":\" $VPN\"}"
+  echo "{\"text\":\"<span color='#7cd380'> $VPN</span>\"}"
 fi
